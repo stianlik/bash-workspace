e35bf3860da0643a352e5d94113ef74457b0471c
Stian Liknes <stianlik@gmail.com>
2011-08-11 16:16:33 +0200

    Changed function prefix to _bws_

--- workspace.sh
+++ workspace.sh
@@ -5,124 +5,130 @@
 # Created: 2011-08-10
 #
 
-export workspace_dir=~/.bash-workspace
+export _bws_dir=~/.bash-workspace
 
-workspace_init() {
-    export workspace_active=default
-    mkdir $workspace_dir > /dev/null 2>&1
-    mkdir $workspace_dir/log> /dev/null 2>&1
-    workspace_load
-    workspace_remove_empty
+_bws_init() {
+    export _bws_active=default
+    mkdir $_bws_dir > /dev/null 2>&1
+    mkdir $_bws_dir/log> /dev/null 2>&1
+    _bws_load
+    _bws_remove_empty
 }
 
-workspace_load() { 
+_bws_load() { 
 # Load workspace from persistent storage
-    source $workspace_dir/active 2> /dev/null
-    unset ${!workspace_link_*}
-    source $workspace_dir/log/$workspace_active 2> /dev/null
+    source $_bws_dir/active 2> /dev/null
+    unset ${!_bws_link_*}
+    source $_bws_dir/log/$_bws_active 2> /dev/null
     if [ $? == 1 ]; then
-        workspace_add_link r "`pwd`"
-        workspace_save
+        if [ $_bws_active == 'default' ]; then
+            _bws_add_link r "`echo ~`"
+        else
+            _bws_add_link r "`pwd`"
+        fi
+        _bws_save
     fi
 }
 
-workspace_save() {
+_bws_save() {
 # Save workspace to persistent storage
-    echo `export -p | grep workspace_link_ | sed -e 's/.*\?workspace_link_/export workspace_link_/g'`>$workspace_dir/log/$workspace_active
+    echo `export -p | grep _bws_link_ | sed -e 's/.*\?_bws_link_/export _bws_link_/g'`>$_bws_dir/log/$_bws_active
 }
 
-workspace_remove_empty() {
+_bws_remove_empty() {
 # Remove empty workspaces except default and active
-    for ws in `ls $workspace_dir/log`; do
-        local file_size=`du -b $workspace_dir/log/$ws | sed -e s/[^0-9]*//g`
-        if [ $file_size -le 1 ] && [ "$ws" != "default" ] && [ "$ws" != $workspace_active ]; then
-            rm "$workspace_dir/log/$ws"
+    for ws in `ls $_bws_dir/log`; do
+        local file_size=`du -b $_bws_dir/log/$ws | sed -e s/[^0-9]*//g`
+        if [ $file_size -le 1 ] && [ "$ws" != "default" ] && [ "$ws" != $_bws_active ]; then
+            rm "$_bws_dir/log/$ws"
         fi;
     done;
 }
 
-workspace_change() {
+_bws_change() {
 # Change workspace
 # @param workspace
-    workspace_save
-    echo "export workspace_active=$1" > $workspace_dir/active
-    workspace_load
+    _bws_save
+    echo "export _bws_active=$1" > $_bws_dir/active
+    _bws_load
 }
 
-workspace_activate() {
+_bws_activate() {
 # Activate workspace
 # @param workspace
-    echo "export workspace_active=$1" > $workspace_dir/active
-    workspace_load
+    echo "export _bws_active=$1" > $_bws_dir/active
+    _bws_load
 }
 
-workspace_empty_active() {
+_bws_empty_active() {
 # Empty the current workspace
-    unset ${!workspace_link_*}
-    workspace_save
+    local root="$_bws_link_r"
+    unset ${!_bws_link_*}
+    export _bws_link_r="$root"
+    _bws_save
 }
 
-workspace_remove() {
+_bws_remove() {
 # Remove workspace
-    unset ${!workspace_link_*}
-    rm $workspace_dir/log/$workspace_active
-    workspace_activate "default"
+    unset ${!_bws_link_*}
+    rm $_bws_dir/log/$_bws_active
+    _bws_activate "default"
 }
 
-workspace_remove_all() {
+_bws_remove_all() {
 # Remove all workspaces
-    unset ${!workspace_link_*}
-    rm $workspace_dir/log/*
-    workspace_activate "default"
-    workspace_save
+    unset ${!_bws_link_*}
+    rm $_bws_dir/log/*
+    _bws_activate "default"
+    _bws_save
 }
 
-workspace_escape() {
+_bws_escape() {
 # Replace illegal characters with underline
 # @param Link name
     echo $1 | sed -e s/[^a-z0-9_]/_/gi
 }
 
-workspace_escape_current_basename() {
+_bws_escape_current_basename() {
     local tmp="`pwd`"
     tmp="`basename "$tmp"`"
-    workspace_escape "$tmp"
+    _bws_escape "$tmp"
 }
 
-workspace_add_link() {
+_bws_add_link() {
 # Add link to active workspace
 # @param Link name
 # @param Target directory
-    workspace_cmd="export workspace_link_$1='$2'"
-    eval $workspace_cmd
-    workspace_save
+    _bws_cmd="export _bws_link_$1='$2'"
+    eval $_bws_cmd
+    _bws_save
 }
 
-workspace_remove_link() {
+_bws_remove_link() {
 # Remove link from active workspace
 # @param Link name
-    local workspace_cmd="unset workspace_link_$1"
-    eval $workspace_cmd
-    workspace_save
+    local _bws_cmd="unset _bws_link_$1"
+    eval $_bws_cmd
+    _bws_save
 }
 
-workspace_change_directory() {
+_bws_change_directory() {
 # @param Link name
-    local workspace_path=workspace_link_$1;
-    workspace_path=${!workspace_path}
-    if [ -n "$workspace_path" ]; then
-        cd $workspace_path
+    local _bws_path=_bws_link_$1;
+    _bws_path=${!_bws_path}
+    if [ -n "$_bws_path" ]; then
+        cd $_bws_path
         return 0
     fi
     return 1
 }
 
-workspace_list() {
+_bws_list() {
 # List workspaces
     local s="[" #local s="\033[40m\033[1;34m"
     local e="]" #local e="\033[0m"
-    for ws in `ls $workspace_dir/log`; do
-        if [ "$ws" == $workspace_active ]; then
+    for ws in `ls $_bws_dir/log`; do
+        if [ "$ws" == $_bws_active ]; then
             echo -n -e "${s}$ws${e} "
         else
             echo -n -e "$ws "
@@ -131,18 +137,18 @@ workspace_list() {
     echo
 }
 
-workspace_list_link_names() {
+_bws_list_link_names() {
 # List links in current workspace
-    export -p | grep workspace_link_ | sed -e 's/.*\?workspace_link_//g' -e 's/=.*//g' | tr "\n" " "
+    export -p | grep _bws_link_ | sed -e 's/.*\?_bws_link_//g' -e 's/=.*//g' | tr "\n" " "
     echo
 }
 
-workspace_list_links() {
+_bws_list_links() {
 # List links in current workspace
-    export -p | grep workspace_link_ | sed -e 's/.*\?workspace_link_//g'
+    export -p | grep _bws_link_ | sed -e 's/.*\?_bws_link_//g'
 }
 
-workspace_confirm() {
+_bws_confirm() {
 # Get user confirmation
 # @param message
 # @return 0 if user answered yes, 1 otherwise
@@ -157,157 +163,156 @@ workspace_confirm() {
     fi
 }
 
-workspace_remove_confirm() {
-    workspace_confirm "Remove active workspace ($workspace_active)"
+_bws_remove_confirm() {
+    _bws_confirm "Remove active workspace ($_bws_active)"
     if [ $? == 1 ]; then
         echo "Nothing done"
         return
     fi
-    workspace_remove
+    _bws_remove
 }
 
-workspace_remove_all_confirm() {
-    workspace_confirm "Remove all workspaces"
+_bws_remove_all_confirm() {
+    _bws_confirm "Remove all workspaces"
     if [ $? == 1 ]; then
         echo "Nothing done"
         return
     fi
-    workspace_remove_all
-    workspace_activate "default"
+    _bws_remove_all
+    _bws_activate "default"
 }
 
-workspace_change_directory_confirm() {
+_bws_change_directory_confirm() {
 # @param Link name
-    workspace_change_directory $1
+    _bws_change_directory $1
     if [ $? == 1 ]; then
         echo "Could not find link ('$1')"
     fi
 }
 
-workspace_empty_active_confirm() { 
+_bws_empty_active_confirm() { 
 # Empty the active workspace (removing all links)
-    workspace_confirm "Empty workspace ($workspace_active)"
+    _bws_confirm "Empty workspace ($_bws_active)"
     if [ $? == 1 ]; then
         echo "Nothing done"
         return
     fi
-    workspace_empty_active
+    _bws_empty_active
 }
 
-workspace_list_commands() {
+_bws_list_commands() {
     echo "cd cw empty help ln ls reset rm"
 }
 
-workspace_helptext() {
+_bws_helptext() {
 # @param Name of command
     echo "Assuming $1 is an alias for \"source /path/to/workspace.sh\""
     echo "Usage: $1                       List workspaces"
     echo "   or: $1 cd [<name>]           Change directory, if <name> is omitted, go to \"r\" (workspace root)"
-    echo "   or: $1 cw [<workspace_name>] Change workspace, if <workspace_name> is omitted, default workspace is activated"
+    echo "   or: $1 cw [<_bws_name>] Change workspace, if <_bws_name> is omitted, default workspace is activated"
     echo "   or: $1 empty                 Empty active workspace (removing all links)"
     echo "   or: $1 help                  Display this.."
     echo "   or: $1 ln <name>             Add link to current directory in active workspace"
     echo "   or: $1 ls                    List all directories in workspace"
     echo "   or: $1 reset                 Remove all workspaces"
     echo "   or: $1 rm [<name>]           Remove directory from workspace, or remove entire workspace if <name> is omitted"
-    echo "   or: $1 start                 Activate workspaces"
 }
 
-workspace_autocomplete() {
+_bws_autocomplete() {
     local cur prev suggestions
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     if [ $COMP_CWORD -eq 1 ]; then
-        suggestions="`workspace_list_commands`"
+        suggestions="`_bws_list_commands`"
     elif [ $COMP_CWORD -eq 2 ]; then
         if [ "$prev" == "cd" ] || [ "$prev" == "rm" ]; then
-            suggestions="`workspace_list_link_names`"
+            suggestions="`_bws_list_link_names`"
         elif [ "$prev" == "cw" ]; then
-            suggestions="`ls $workspace_dir/log` `workspace_escape_current_basename`"
+            suggestions="`ls $_bws_dir/log` `_bws_escape_current_basename`"
         elif [ "$prev" == "ln" ]; then
-            suggestions="`workspace_escape_current_basename`"
+            suggestions="`_bws_escape_current_basename`"
         fi
     fi
     COMPREPLY=(`compgen -W "${suggestions}" $cur`)
 }
 
-workspace_run() {
+_bws_run() {
 # @param command
 # @param name
-    workspace_init
+    _bws_init
 
     local me="bws"
-    local workspace_command="$1"
-    local workspace_name="$2"
+    local _bws_command="$1"
+    local _bws_name="$2"
 
     # Add
-    if [ "$workspace_command" == 'ln' ]; then
-        if [ -n "$workspace_name" ]; then
-            workspace_add_link `workspace_escape "$workspace_name"` "`pwd`"
+    if [ "$_bws_command" == 'ln' ]; then
+        if [ -n "$_bws_name" ]; then
+            _bws_add_link `_bws_escape "$_bws_name"` "`pwd`"
         else
-            workspace_helptext $me
+            _bws_helptext $me
         fi
 
     # Remove
-    elif [ "$workspace_command" == 'rm' ]; then
-        if [ -n "$workspace_name" ]; then
-            workspace_remove_link `workspace_escape "$workspace_name"`
+    elif [ "$_bws_command" == 'rm' ]; then
+        if [ -n "$_bws_name" ]; then
+            _bws_remove_link `_bws_escape "$_bws_name"`
         else
-            workspace_remove_confirm
+            _bws_remove_confirm
         fi
 
     # Change directory
-    elif [ "$workspace_command" == 'cd' ]; then
-        if [ -n "$workspace_name" ]; then
-            workspace_change_directory_confirm `workspace_escape "$workspace_name"`
+    elif [ "$_bws_command" == 'cd' ]; then
+        if [ -n "$_bws_name" ]; then
+            _bws_change_directory_confirm `_bws_escape "$_bws_name"`
         else
-            workspace_change_directory_confirm r # Workspace root dir
+            _bws_change_directory_confirm r # Workspace root dir
         fi
 
     # Change workspace
-    elif [ "$workspace_command" == 'cw' ]; then
-        if [ -n "$workspace_name" ]; then
-            workspace_change `workspace_escape "$workspace_name"`
+    elif [ "$_bws_command" == 'cw' ]; then
+        if [ -n "$_bws_name" ]; then
+            _bws_change `_bws_escape "$_bws_name"`
         else
-            workspace_change default
+            _bws_change default
         fi
 
     # Reset (remove all workspaces)
-    elif [ "$workspace_command" == 'reset' ]; then
-        workspace_remove_all_confirm `workspace_escape "$workspace_name"`
+    elif [ "$_bws_command" == 'reset' ]; then
+        _bws_remove_all_confirm `_bws_escape "$_bws_name"`
 
     # Empty workspace
-    elif [ "$workspace_command" == 'empty' ]; then
-        workspace_empty_active_confirm
+    elif [ "$_bws_command" == 'empty' ]; then
+        _bws_empty_active_confirm
 
     # List links
-    elif [ "$workspace_command" == 'ls' ]; then
-        workspace_list_links
+    elif [ "$_bws_command" == 'ls' ]; then
+        _bws_list_links
 
     # Help
-    elif [ "$workspace_command" == 'help' ]; then
-        workspace_helptext $me
+    elif [ "$_bws_command" == 'help' ]; then
+        _bws_helptext $me
 
     # List workspaces
-    elif [ -z "$workspace_command" ]; then
-        workspace_list
+    elif [ -z "$_bws_command" ]; then
+        _bws_list
 
     # Autocomplete
-    elif [ "$workspace_command" == 'autocomplete' ]; then
-        if [ -z "$workspace_name" ]; then workspace_name=$me; fi;
-        complete -F workspace_autocomplete "$workspace_name"
+    elif [ "$_bws_command" == 'autocomplete' ]; then
+        if [ -z "$_bws_name" ]; then _bws_name=$me; fi;
+        complete -F _bws_autocomplete "$_bws_name"
 
     # Run workspace function directly
-    elif [ "$workspace_command" == '_func' ]; then
-        local func=`workspace_escape "$workspace_name"`
-        workspace_`eval echo $func`
+    elif [ "$_bws_command" == '_func' ]; then
+        local func=`_bws_escape "$_bws_name"`
+        _bws_`eval echo $func`
 
     # Default
     else
-        workspace_helptext $me
+        _bws_helptext $me
     fi
 }
 
-workspace_run "$1" "$2"
+_bws_run "$1" "$2"
